60c1fb0c1ddda865cddc46654133cca217abefb1
Mina Her <minacle@live.com>
2022-11-19 13:27:10 +0900

    Make to reveal boosts+favourites counters on every toot

--- app/javascript/mastodon/features/hashtag_timeline/index.js
+++ app/javascript/mastodon/features/hashtag_timeline/index.js
@@ -224,6 +224,7 @@ class HashtagTimeline extends React.PureComponent {
           onLoadMore={this.handleLoadMore}
           emptyMessage={<FormattedMessage id='empty_column.hashtag' defaultMessage='There is nothing in this hashtag yet.' />}
           bindToDocument={!multiColumn}
+          withCounters
         />
 
         <Helmet>
