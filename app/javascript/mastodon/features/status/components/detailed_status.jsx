ab1758ee97d0762400d1da415f237459a5e8b652
Mina Her <minacle@live.com>
2022-11-23 08:28:09 +0900

    Show language name of statuses

--- app/javascript/mastodon/features/status/components/detailed_status.jsx
+++ app/javascript/mastodon/features/status/components/detailed_status.jsx
@@ -151,6 +151,7 @@ class DetailedStatus extends ImmutablePureComponent {
     let reblogIcon = 'retweet';
     let favouriteLink = '';
     let edited = '';
+    let languageCode = '';
     let replies = '';
 
     if (this.props.measureHeight) {
@@ -293,6 +294,25 @@ class DetailedStatus extends ImmutablePureComponent {
       );
     }
 
+    const originalLanguage = status.get('language');
+    if (language !== undefined && originalLanguage !== null && Intl) {
+      try {
+        languageCode = (
+          <>
+            {' · '}
+            <>{new Intl.DisplayNames([intl.locale], { type: 'language' }).of(originalLanguage)}</>
+          </>
+        );
+      } catch {
+        languageCode = (
+          <>
+            {' · '}
+            {originalLanguage}
+          </>
+        );
+      }
+    }
+
     replies = (
       <>
         {' · '}
@@ -335,7 +355,7 @@ class DetailedStatus extends ImmutablePureComponent {
           <div className='detailed-status__meta'>
             <a className='detailed-status__datetime' href={`/@${status.getIn(['account', 'acct'])}/${status.get('id')}`} target='_blank' rel='noopener noreferrer'>
               <FormattedDate value={new Date(status.get('created_at'))} hour12={false} year='numeric' month='short' day='2-digit' hour='2-digit' minute='2-digit' />
-            </a>{edited}{visibilityLink}{applicationLink}{replies}{reblogLink} · {favouriteLink}
+            </a>{edited}{visibilityLink}{languageCode}{applicationLink}{replies}{reblogLink} · {favouriteLink}
           </div>
         </div>
       </div>
