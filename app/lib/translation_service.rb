d01e394193ce9cee8e69e5c5a92965a372ae318f
Mina Her <minacle@live.com>
2023-02-24 21:54:25 +0900

    Implement Google Translate as Translation Service

--- app/lib/translation_service.rb
+++ app/lib/translation_service.rb
@@ -12,13 +12,15 @@ class TranslationService
       TranslationService::DeepL.new(ENV.fetch('DEEPL_PLAN', 'free'), ENV['DEEPL_API_KEY'])
     elsif ENV['LIBRE_TRANSLATE_ENDPOINT'].present?
       TranslationService::LibreTranslate.new(ENV['LIBRE_TRANSLATE_ENDPOINT'], ENV['LIBRE_TRANSLATE_API_KEY'])
+    elsif ENV['GOOGLE_TRANSLATE_API_KEY'].present?
+      TranslationService::GoogleTranslateV2.new(ENV['GOOGLE_TRANSLATE_API_KEY'])
     else
       raise NotConfiguredError
     end
   end
 
   def self.configured?
-    ENV['DEEPL_API_KEY'].present? || ENV['LIBRE_TRANSLATE_ENDPOINT'].present?
+    ENV['DEEPL_API_KEY'].present? || ENV['LIBRE_TRANSLATE_ENDPOINT'].present? || ENV['GOOGLE_TRANSLATE_API_KEY'].present?
   end
 
   def languages
